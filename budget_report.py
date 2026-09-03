0d6bb1ed2919542d66c151f91d93704e8cd8ee8d
Emmanuel Nweke <nwekechineduemmanuel9@gmail.com>
2026-09-03 12:09:58 +0100

    update currency converter and budget report

--- budget_report.py
+++ budget_report.py
@@ -1,8 +1,9 @@
-import json
+import json 
 import csv
 
 
 class BudgetReport:
+#This is the class for the budget report
 
     @staticmethod
     def save_trip_json(trip, filename="trip.json"):
@@ -14,7 +15,14 @@ class BudgetReport:
             "end_date": trip.end_date.isoformat(),
             "duration_days": trip.duration_days,
         }
-
+        # data = {
+        #     "home_currency": trip.home_currency,
+        #     "destination_currency": trip.destination_currency,
+        #     "amount": trip.amount,
+        #     "start_date": trip.start_date.isoformat(),
+        #     "end_date": trip.end_date.isoformat(),
+        #     "duration_days": trip.duration_days,
+        # }
         with open(filename, "w", encoding="utf-8") as file:
             json.dump(data, file, indent=4)
 
